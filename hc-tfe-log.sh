240b7d64e1768753dcf4274f8759c501e7db1872
ml4 <ml4@psy.industries>
2022-05-18 20:41:15 +0100

    Fix TFE log script for clearer output

--- hc-tfe-log.sh
+++ hc-tfe-log.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+## hc-tfe-log.sh
+#
+## 2022-05-18 ml4
+## TESTED WITH 610
+#
+################################################################################################################
 
 function usage {
   echo "Usage: $(basename ${0}) <log group> <region>"
@@ -7,39 +14,15 @@ function usage {
   exit 1
 }
 
-function log {
-  bldred="\033[0;31m" # Red
-  bldgrn="\033[0;32m" # Green
-  bldylw="\033[0;33m" # Yellow
-  bldblu="\033[0;34m" # Blue
-  bldpur="\033[0;35m" # Purple
-  bldcyn="\033[0;36m" # Cyan
-  bldwht="\033[0;37m" # White
-  txtrst="\033[0m"    # Text Reset
-
-  local -r level="$1"
-  if [ "${level}" == "INFO" ]
-  then
-    COL=${bldgrn}
-  elif [ "${level}" == "ERROR" ]
-  then
-    COL=${bldred}
-  elif [ "${level}" == "WARN" ]
-  then
-    COL=${bldylw}
-  fi
-
-  local -r func="$2"
-  local -r message="$3"
-  local -r timestamp=$(date +"%Y-%m-%d %H:%M:%S")
-  >&2 echo -e "${bldcyn}${timestamp}${txtrst} [${COL}${level}${txtrst}] [${SCRIPT_NAME}:${func}] ${message}"
-}
-
 function main {
+  nmlred="\033[0;31m" # Red
   bldred="\033[1;31m" # Red
+  nmlgrn="\033[0;32m" # Green
   bldgrn="\033[1;32m" # Green
+  nmlylw="\033[0;33m" # Yellow
   bldylw="\033[1;33m" # Yellow
   bldblu="\033[1;34m" # Blue
+  nmlpur="\033[0;35m" # Purple
   bldpur="\033[1;35m" # Purple
   bldcyn="\033[1;36m" # Cyan
   nmlcyn="\033[0;36m" # Cyan
@@ -54,37 +37,123 @@ function main {
     usage
   fi
 
-  aws logs tail ${group} --follow --region ${region} | while read line
+  aws logs tail ${group} --follow --region ${region} | while read -r line
   do
-    timestamp=$(echo ${line} | awk '{print $1}')
-    tag=$(echo ${line} | /usr/bin/sed 's/.*CONTAINER_TAG":"\([^"]*\)".*/\1/g')
-    tag_first_ch=$(echo ${tag} | cut -c1)
-    if [[ ${tag_first_ch} =~ ^[0-9]+$ ]]
-    then
-      ## it means sed did not find CONTAINER_TAG so it must be a SYSTEM-type log output
-      #
-      tag='SYSTEM'
-    fi
-    message=$(echo ${line} | /usr/bin/sed 's/.*"message":"\(.*\)"_SOURCE_REALTIME_TIMESTAMP.*/\1/')
+    log=$(echo "${line}" | cut -d' ' -f3-)
+    container_name=$(echo "${log}" | /usr/bin/sed 's/.*container_tag":"\([^"]*\)".*/\1/g')
+    message=$(echo "${log}" | jq -r .message)
 
-    level=${bldpur}
-    if [[ ${message} =~ '[ERROR]' ]]
+    ## normalise timestamp
+    ## there are several different formats due to conglomeration of services within TFE:
+    #
+    ## "message":"10.128.21.133 - - [18/May/2022:12:11:15 +0000] ...
+    ## "message":"[2022/05/18 12:21:13] [error] ...
+    ## "message":"2022-05-18 12:21:05 [INFO] ...
+    ## "message":"  [DEPRECATION] :after_commit AASM callback is not safe in terms of race conditions and redundant calls.","source_realtime_timestamp":"1652888678750007"
+    #
+    if [[ $(echo ${message} | awk '{print $1}') == '[DEPRECATION]' ]]
     then
-      level=${bldred}
-    elif [[ ${message} =~ '[WARN]' ]]
+      ## Deprecation warning - take timestamp from line, not message
+      #
+      timestamp=$(echo ${message} | awk '{print $1}' | sed 's/T/ /' | sed 's/\..*//')
+      logEntry="${message}"
+      level="DPRC8"
+    elif [[ $(echo ${message} | awk '{print $1}') =~ ^[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}$ ]]
     then
-      level=${bldylw}
-    elif [[ ${message} =~ '[DEBUG]' ]]
+      ## nginx-type log message
+      #
+      ts=$(echo ${message} | cut -d' ' -f4)
+      day=$(echo ${ts} | cut -d'/' -f1 | tr -d '[')
+      month=$(echo ${ts} | cut -d'/' -f2)
+      year=$(echo ${ts} | cut -d'/' -f3 | cut -d: -f1)
+      time=$(echo ${ts} | cut -d: -f2-4)
+      case ${month} in
+        January) mo='01' ;;
+        February) mo='02' ;;
+        March) mo='03' ;;
+        April) mo='04' ;;
+        May) mo='05' ;;
+        June) mo='06' ;;
+        July) mo='07' ;;
+        August) mo='08' ;;
+        September) mo='09' ;;
+        October) mo='10' ;;
+        November) mo='11' ;;
+        December) mo='12' ;;
+        *)
+          echo "ERROR IN SCRIPT"
+          exit 1
+          ;;
+      esac
+      timestamp="${year}-${mo}-${day} ${time}"
+      logEntry=$(echo "${message}" | cut -d' ' -f6-)
+      level=$(echo "${message}" | cut -d' ' -f9 | tr -d '[]')
+    elif [[ $(echo ${message} | awk '{print $1}') =~ [0-9]{4}-[0-9]{2}-[0-9]{2}T ]]
     then
-      level=${nmlwht}
-    elif [[ ${message} =~ '[Audit Log]' ]]
+      ## sidekiq-type log message
+      #
+      timestamp=$(echo ${message} | cut -d' ' -f1 | sed 's/T/ /' | sed 's/\.[0-9][0-9][0-9]Z//')
+      level=$(echo "${message}" | awk '{print $6}' | tr -d ':')
+      logEntry=$(echo "${message}" | cut -d' ' -f2-)
+    elif [[ "${message:0:1}" == '[' ]]
     then
-      level=${bldblu}
-    elif [[ "${tag}" == 'SYSTEM' ]]
-    then
-      level=${nmlcyn}
+      ## fluent-bit-type entries
+      #
+      timestamp=$(echo ${message} | cut -d' ' -f1-2 | tr -d '[]')
+      level=$(echo "${message}" | cut -d'[' -f3 | tr -d '[]' | awk '{print $1}')
+      logEntry=$(echo "${message}" | cut -d']' -f3- | sed 's/^ //')
+    else
+      ## "normal" entries
+      #
+      timestamp=$(echo ${message} | cut -d' ' -f1-2 | tr -d '[]')
+      level=$(echo "${message}" | awk '{print $3}' | tr -d '[]')
+      logEntry=$(echo "${message}" | cut -d' ' -f4-)
     fi
-    echo -e "${nmlcyn}${timestamp} ${txtrst}| ${bldgrn}${tag} ${txtrst}| ${level}${message}"
+
+    colour=${bldwht}
+    level=$(echo ${level} | tr '[:lower:]' '[:upper:]')
+    if [[ ${level} =~ 'ERROR' ]]
+    then
+      colour=${bldred}
+    elif [[ ${level} =~ 'WARN' ]]
+    then
+      colour=${bldylw}
+    elif [[ ${level} =~ 'DEBUG' ]]
+    then
+      colour=${nmlwht}
+    elif [[ "${level}" =~ 'INFO' ]]
+    then
+      colour=${bldwht}
+    elif [[ "${level}" =~ 'DPRC8' ]]
+    then
+      colour=${nmlylw}
+    elif [[ ${line} =~ 'Audit Log' ]]
+    then
+      colour=${bldblu}
+    elif [[ ${level:0:1} =~ [12] ]]
+    then
+      colour=${bldgrn}
+    elif [[ ${level:0:1} =~ [3] ]]
+    then
+      colour=${nmlgrn}
+    elif [[ ${level:0:1} =~ [4] ]]
+    then
+      colour=${nmlred}
+    elif [[ ${level:0:1} =~ [5] ]]
+    then
+      colour=${bldred}
+    else
+      ## We probably encountered a message which we've not accounted for - output special case
+      #
+      colour=${nmlpur}
+      logEntry=${message}
+      level="?????"
+    fi
+
+    # echo '--------------------------'
+    # echo "$line"
+    # echo '--------------------------'
+    printf "${nmlcyn}%s${txtrst} ${bldgrn}%14s${txtrst} [${colour}%5s${txtrst}] ${colour}%s${txtrst}\n" "${timestamp}" "${container_name}" "${level}" "${logEntry}"
   done
 }
 
