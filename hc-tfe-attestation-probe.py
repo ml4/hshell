975b2e6ef2029c76988116cb75c966d06b3904b3
ml4 <ml4@psy.industries>
2022-06-15 15:39:43 +0100

    Add release check, fix colours, add initial cv handling

--- hc-tfe-attestation-probe.py
+++ hc-tfe-attestation-probe.py
@@ -75,8 +75,10 @@ class bcolors:
 ## output a line the width of the terminal
 #
 def line():
+  print(f'{bcolors.Default}')
   line = '#' * int(columns)
   print(line)
+  print()
 #
 ## End Func line
 
@@ -120,11 +122,24 @@ def callTFE(QUIET, DEBUG, path):
 def runReport(QUIET, DEBUG, org):
   if not QUIET:
     line()
-    print(f'{bcolors.Default}TFE Address:      {bcolors.BWhite}{TFE_ADDR}{bcolors.Endc}')
-    print(f'{bcolors.Default}TFE CA Cert file: {bcolors.BWhite}{TFE_CACERT}{bcolors.Endc}')
+    print(f'{bcolors.Green}TFE.{bcolors.Default}Address:         {bcolors.BWhite}{TFE_ADDR}{bcolors.Endc}')
+    print(f'{bcolors.Green}TFE.{bcolors.Default}CA Cert file:    {bcolors.BWhite}{TFE_CACERT}{bcolors.Endc}')
     if DEBUG:
-      print(f'{bcolors.Default}TFE Token:        {bcolors.BWhite}{TFE_TOKEN}{bcolors.Endc}')
+      print(f'{bcolors.Green}TFE.{bcolors.Default}TFE Token:       {bcolors.BWhite}{TFE_TOKEN}{bcolors.Endc}')
+
+  ## Get TFE version and ensure it is recent enough to download config versions
+  #
+  releaseBlob = callTFE(QUIET, DEBUG, f'/admin/release')
+  print(f'{bcolors.Green}TFE.{bcolors.Default}Release:         {bcolors.BMagenta}{releaseBlob["release"]}{bcolors.Endc}')
+  print
+  yearMonth = int(releaseBlob["release"][1:7])
+  if yearMonth < 202203:
     print()
+    print(f'{bcolors.BRed}ERROR: Your TFE release version ({releaseBlob["release"]}) needs to be >= 202203-1 in order to be able to download the configuration versions required to putative understand changes. Exiting here')
+    exit(1)
+  if not QUIET:
+    line()
+
 
   ## Initial workspace items
   #
@@ -142,18 +157,18 @@ def runReport(QUIET, DEBUG, org):
       'resource-count':      f'{array_obj["attributes"]["resource-count"]}',
     }
   for key in sorted(workspaces):
-    print(f'{bcolors.Green}workspace.{bcolors.BGreen}Name:                {bcolors.BMagenta}{key}{bcolors.Endc}')
-    print(f'{bcolors.Green}workspace.{bcolors.BGreen}ID:                  {bcolors.BCyan}{workspaces[key]["id"]}{bcolors.Endc}')
-    print(f'{bcolors.Green}workspace.{bcolors.BGreen}TF Version:          {workspaces[key]["terraform-version"]}{bcolors.Endc}')
-    print(f'{bcolors.Green}workspace.{bcolors.BGreen}Created:             {workspaces[key]["created-at"]}{bcolors.Endc}')
+    print(f'{bcolors.Green}workspace.{bcolors.Default}Name:                {bcolors.BMagenta}{key}{bcolors.Endc}')
+    print(f'{bcolors.Green}workspace.{bcolors.Default}ID:                  {bcolors.BCyan}{workspaces[key]["id"]}{bcolors.Endc}')
+    print(f'{bcolors.Green}workspace.{bcolors.Default}TF Version:          {workspaces[key]["terraform-version"]}{bcolors.Endc}')
+    print(f'{bcolors.Green}workspace.{bcolors.Default}Created:             {workspaces[key]["created-at"]}{bcolors.Endc}')
     if workspaces[key]["locked"] == "True":
       colour = f'{bcolors.BRed}'
     else:
-      colour = f'{bcolors.BGreen}'
-    print(f'{bcolors.Green}workspace.{bcolors.BGreen}Locked:              {colour}{workspaces[key]["locked"]}{bcolors.Endc}')
-    print(f'{bcolors.Green}workspace.{bcolors.BGreen}Speculative Enabled: {workspaces[key]["speculative-enabled"]}{bcolors.Endc}')
-    print(f'{bcolors.Green}workspace.{bcolors.BGreen}Global Remote State: {workspaces[key]["global-remote-state"]}{bcolors.Endc}')
-    print(f'{bcolors.Green}workspace.{bcolors.BGreen}Resources in State:  {workspaces[key]["resource-count"]}{bcolors.Endc}')
+      colour = f'{bcolors.Default}'
+    print(f'{bcolors.Green}workspace.{bcolors.Default}Locked:              {colour}{workspaces[key]["locked"]}{bcolors.Endc}')
+    print(f'{bcolors.Green}workspace.{bcolors.Default}Speculative Enabled: {workspaces[key]["speculative-enabled"]}{bcolors.Endc}')
+    print(f'{bcolors.Green}workspace.{bcolors.Default}Global Remote State: {workspaces[key]["global-remote-state"]}{bcolors.Endc}')
+    print(f'{bcolors.Green}workspace.{bcolors.Default}Resources in State:  {workspaces[key]["resource-count"]}{bcolors.Endc}')
     #
     ## Run data
     #
@@ -167,15 +182,65 @@ def runReport(QUIET, DEBUG, org):
       else:
         print(f'{bcolors.Green}run.{bcolors.BCyan}Created by:                {bcolors.BRed}No user found!{bcolors.Endc}')
 
-      if runBlob["data"][0]["attributes"]["canceled-at"]:
-        print(f'{bcolors.Green}run.{bcolors.BCyan}Canceled:                  {bcolors.BYellow}{runBlob["data"][0]["attributes"]["canceled-at"]}{bcolors.Endc}')
+      if runBlob["data"][0]["relationships"]["configuration-version"]["data"]["id"]:
+        print(f'{bcolors.Green}run.{bcolors.BCyan}Configuration Version:     {bcolors.BBlue}{runBlob["data"][0]["relationships"]["configuration-version"]["data"]["id"]}{bcolors.Endc}')
       else:
+        print(f'{bcolors.Green}run.{bcolors.BCyan}Configuration Version:     {bcolors.BRed}No configuration version found!{bcolors.Endc}')
+      #
+      ## if we have a configuration that begins with 'cv', hit the API again and get the configuration version data:
+      ##   - as we have a configuration version, get a list of configuration versions and ensure there are two
+      ##   - get the previous configuration version
+      ##   - show both configuration versions = get links to the blobs containing the configuration data
+      ##   - get each blob
+      ##   - diff the blobs and output
+      #
+      try:
+        if runBlob["data"][0]["relationships"]["configuration-version"]["data"]["id"].startswith("cv-"):
+          cvListBlob = callTFE(QUIET, DEBUG, f'/workspaces/{workspaces[key]["id"]}/configuration-versions')
+
+          if len(cvListBlob) == 0:
+            print(f'{bcolors.BRed}ERROR: Configuration version list blob is empty, but configuration version {runBlob["data"][0]["relationships"]["configuration-version"]["data"]["id"]} detected.{bcolors.Endc}. Exiting here')
+            exit(1)
+          elif len(cvListBlob) == 1:
+            firstCV = True  # see below when we diff the blobs
+          else:
+            multipleCV = True
+      except KeyError:
+        print(f'{bcolors.Green}run.{bcolors.BCyan}Configuration Versions:    {bcolors.BRed}List Not Found{bcolors.Endc}')
+
+      if multipleCV == True:
+        # OK we have >1 configuration versions: get the second one in the array (1) - we already have element 0, but check
+        if runBlob["data"][0]["relationships"]["configuration-version"]["data"]["id"] != cvListBlob["data"][0]["id"]:
+          print(f'{bcolors.BRed}ERROR: Configuration version ({runBlob["data"][0]["relationships"]["configuration-version"]["data"]["id"]}) is different from element 0 in the configuration versions blob ({cvListBlob["data"][0]["id"]}).{bcolors.Endc}. Exiting here')
+          exit(1)
+        cv0 = cvListBlob["data"][0]["id"]
+        cv0download = cvListBlob["data"][0]["links"]["download"]
+        cv1 = cvListBlob["data"][1]["id"]
+        cv1download = cvListBlob["data"][1]["links"]["download"]
+
+        if cv0 and cv0download and cv1 and cv1download:
+          print(f'{bcolors.Green}run.{bcolors.BCyan}Latest CV:                 {bcolors.BCyan}{cv0}{bcolors.Endc}')
+          print(f'{bcolors.Green}run.{bcolors.BCyan}Latest CV Download:        {bcolors.BCyan}{cv0download}{bcolors.Endc}')
+          print(f'{bcolors.Green}run.{bcolors.BCyan}Previous CV:               {bcolors.BCyan}{cv1}{bcolors.Endc}')
+          print(f'{bcolors.Green}run.{bcolors.BCyan}Previous CV Download:      {bcolors.BCyan}{cv1download}{bcolors.Endc}')
+
+        cv0blob = callTFE(QUIET, DEBUG, f'/configuration-version/{cv0}/download')
+        cv1blob = callTFE(QUIET, DEBUG, f'/configuration-version/{cv1}/download')
+
+        print(f'{cv0blob}')
+        print()
+        print(f'{cv1blob}')
+        print()
+
+      try:
+        print(f'{bcolors.Green}run.{bcolors.BCyan}Canceled:                  {bcolors.BYellow}{runBlob["data"][0]["attributes"]["canceled-at"]}{bcolors.Endc}')
+      except KeyError:
         print(f'{bcolors.Green}run.{bcolors.BCyan}Canceled:                  {bcolors.BCyan}Not canceled{bcolors.Endc}')
 
-      if runBlob["data"][0]["attributes"]["created-at"] is None:
-        print(f'{bcolors.Green}run.{bcolors.BCyan}Created:                   {bcolors.BYellow}Not Created{bcolors.Endc}')
-      else:
+      try:
         print(f'{bcolors.Green}run.{bcolors.BCyan}Created:                   {bcolors.BCyan}{runBlob["data"][0]["attributes"]["created-at"]}{bcolors.Endc}')
+      except KeyError:
+        print(f'{bcolors.Green}run.{bcolors.BCyan}Created:                   {bcolors.BYellow}Not Created{bcolors.Endc}')
 
       try:
         print(f'{bcolors.Green}run.{bcolors.BCyan}Plan Queueable:            {bcolors.BCyan}{runBlob["data"][0]["attributes"]["status-timestamps"]["plan-queueable-at"]}{bcolors.Endc}')
@@ -213,9 +278,17 @@ def runReport(QUIET, DEBUG, org):
         print(f'{bcolors.Green}run.{bcolors.BCyan}Confirmed:                 {bcolors.BYellow}Not Confirmed{bcolors.Endc}')
 
       try:
-        print(f'{bcolors.Green}run.{bcolors.BCyan}Applied:                   {bcolors.BCyan}{runBlob["data"][0]["attributes"]["status-timestamps"]["applied-at"]} OUTCOME: {bcolors.BGreen}APPLIED{bcolors.Endc}')
+        print(f'{bcolors.Green}run.{bcolors.BCyan}Applied:                   {bcolors.BCyan}{runBlob["data"][0]["attributes"]["status-timestamps"]["applied-at"]}')
       except KeyError:
-        print(f'{bcolors.Green}run.{bcolors.BCyan}Applied:                   {bcolors.BCyan}OUTCOME: {bcolors.BYellow}NOT APPLIED{bcolors.Endc}')
+        print(f'{bcolors.Green}run.{bcolors.BCyan}Applied:                   {bcolors.BYellow}Not Applied{bcolors.Endc}')
+
+      try:
+        if runBlob["data"][0]["attributes"]["status"] == "applied":
+          print(f'{bcolors.Green}run.{bcolors.BCyan}Status (Outcome):          {bcolors.Default}applied{bcolors.Endc}')
+        else:
+          print(f'{bcolors.Green}run.{bcolors.BCyan}Status (Outcome):          {bcolors.BYellow}{runBlob["data"][0]["attributes"]["status"]}{bcolors.Endc}')
+      except KeyError:
+        print(f'{bcolors.Green}run.{bcolors.BCyan}Status (Outcome):          {bcolors.BRed}UNKNOWN{bcolors.Endc}')
     print()
   if not QUIET:
     print()
